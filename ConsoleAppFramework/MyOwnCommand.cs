4c457b8f0485454a3df0c692966f779ad3e13417
sttimort <mihail.grb@gmail.com>
2017-11-09 19:55:46 +0300

    Add a bit of explanation in MyOwnCommand.cs file

--- ConsoleAppFramework/MyOwnCommand.cs
+++ ConsoleAppFramework/MyOwnCommand.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace Application
 {
-    [Command("main",
+    [Command("main", // this is name of UI Context to which this command belongs
              Name = "mycommand",
              ArgsFormat = "<my arguents discription",
              Aliases = new string[] {"myAlias1, myAlias2"},
@@ -11,9 +11,14 @@ namespace Application
         public MyOwnCommand(string args)
             :base(args)
         {
-            // check of arguments here
+            // args is everything that was typed after command name (or it's alias)
+            // for example for command: mycommands 1, 22f wefwef
+            // args will be: " 1, 22f wefwef"
+
+
+            // check arguments here
             // if they are wrong, throw CommandCreationException(ERROR_MESSAGE)
-            // ERROR_MESSAGE will be displayes to the user
+            //ERROR_MESSAGE will be displayes to the user
         }
 
         public override void Execute(App app)
